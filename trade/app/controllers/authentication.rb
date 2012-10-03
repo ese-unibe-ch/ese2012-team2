ecc8016741b05e95c31784ddc6aa693bc02c7a71
shaechler <babemusicstar@hotmail.com>
2012-10-03 18:50:03 +0200

    commented the authentication controller

--- trade/app/controllers/authentication.rb
+++ trade/app/controllers/authentication.rb
@@ -2,30 +2,32 @@ require 'haml'
 require './models/user'
 class Authentication < Sinatra::Application
 
+  #SH The normal login page
   get "/login" do
     haml :login, :locals =>{:error => nil}
   end
 
+  #SH The login page when an error occurred
   get "/login/:error" do
     haml :login , :locals =>{:error => params[:error]}
   end
 
+  #SH Checks whether login was successful and if so, log the user in.
+  #SH Otherwise, redirect to the error page
   post "/login" do
     name = params[:username]
     password = params[:password]
-
-
-
-      user = Models::User.by_name(name)
-      if user.nil? || password != name
-        redirect '/login/wrong'
-      else
-        session[:name] = name
-        redirect '/'
-      end
+    user = Models::User.by_name(name)
+    if user.nil? || password != name
+      redirect '/login/wrong'
+    else
+      session[:name] = name
+      redirect '/'
+    end
 
   end
 
+  #SH Logs the user out
   get "/logout" do
     session[:name] = nil
     redirect '/login'
