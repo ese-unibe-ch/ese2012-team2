bd198c526ecbbfe7f0b83a420d09e314881a6b0a
shaechler <babemusicstar@hotmail.com>
2012-10-03 18:45:48 +0200

    commented item model

--- trade/app/models/item.rb
+++ trade/app/models/item.rb
@@ -1,20 +1,24 @@
 module Models
   class Item
-    @@items = Array.new
+    @@items = Array.new #SH A list of all items
     attr_accessor :name, :price, :owner, :active
 
+    #SH Returns all items
     def self.all
       @@items
     end
 
+    #SH Gets an item by its name
     def self.by_name(name)
       @@items.detect {|item| item.name == name }
     end
 
+    #SH Deletes an item from the item list
     def self.delete_item(item)
       @@items.delete(item)
     end
 
+    #SH Creates a new item with a name, a price and an owner
     def self.named(name, price, owner)
       item = self.new(price, owner)
       item.name = name
@@ -22,6 +26,8 @@ module Models
       item
     end
 
+    #SH Sets the name, the price, and the owner of the item
+    #SH If the user is not nil, adds the item to the item list of the owner
     def initialize(price, owner)
       self.price = price
       self.owner = owner
