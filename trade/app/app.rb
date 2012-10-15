7aaaf2a1b436f70e5db6984f353836524b1a38cd
Kenneth Radunz <kenneth.radunz@web.de>
2012-10-15 13:38:31 +0200

    Merged email branch into master. This branch solves issue #6. A visitor can now reset a password of user by entering its name. A notification email with the new password will send to the address specified by the users email attribute.
    Squashed commit of the following:
    
    commit 28a139c4713c8dcc77bc03198d574542606deb32
    Merge: 270c525 1c25e0b
    Author: Kenneth Radunz <kenneth.radunz@web.de>
    Date:   Mon Oct 15 13:37:44 2012 +0200
    
        Merge branch 'master' into email
    
    commit 270c52520d17f90aebf61f4af762e046c40ccda3
    Author: Kenneth Radunz <kenneth.radunz@web.de>
    Date:   Mon Oct 15 13:35:42 2012 +0200
    
        each user has now an email address.
        The reset email controller now sends the new password to the user's address
    
    commit 374eb414c7c8458f63b05221f0cb206ed563632a
    Author: Kenneth Radunz <kenneth.radunz@web.de>
    Date:   Thu Oct 11 10:40:24 2012 +0100
    
        implemented a random password generator
    
    commit 20dbd2a31818950a07fce2a07d118836fbae528b
    Author: Kenneth Radunz <kenneth.radunz@web.de>
    Date:   Wed Oct 10 22:10:17 2012 +0200
    
        If the reset user is successfull the user receives a mail with the new password.
        Still need to add a pw generator
        and add emails to the user
    
    commit 3504b2b9c3a209f68b6e25b015e2e759612140a2
    Author: Kenneth Radunz <kenneth.radunz@web.de>
    Date:   Wed Oct 10 21:50:11 2012 +0200
    
        started on reset_password view and controller
    
    commit 2578eaa58e6ff6d3ff5f56863c8de3dfb4156736
    Author: Kenneth Radunz <kenneth.radunz@web.de>
    Date:   Wed Oct 10 17:49:52 2012 +0200
    
        sending an email is not so difficult

--- trade/app/app.rb
+++ trade/app/app.rb
@@ -7,6 +7,7 @@ require_relative 'controllers/main'
 require_relative 'controllers/authentication'
 require_relative 'controllers/item_controller'
 require_relative 'controllers/change_password'
+require_relative 'controllers/reset_password'
 require_relative 'models/user'
 require_relative 'models/item'
 require_relative 'models/data_overlay'
@@ -17,6 +18,7 @@ class App < Sinatra::Base
   use Main
   use ItemController
   use ChangePassword
+  use ResetPassword
 
   enable :sessions
   set :show_exceptions, false
@@ -26,20 +28,20 @@ class App < Sinatra::Base
   configure :development do
     #now use the DataOverlay
     overlay = Models::DataOverlay.instance
-    user1 = overlay.new_user "Hat man", "pwHatman"
+    user1 = overlay.new_user "Hat man", "pwHatman", "hatman@udontsay.org"
     overlay.new_item "Ghastly gibus", 10, "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.", user1, true
     overlay.new_item "Ye olde baker boy", 15, "aslkghdk jfghöa dkfhgsödkjghsöd kjfhgsödkhgös dhsödlkjghös fklgjhsdkljg fsödlkgjh ösflkg hjs fklöghjsödkl gfjsödkljghösdklfgjhk lösfgh  jklösf jghklö sfgjhöfljghl öfgjhlösfjghslöfk jhöe rnbizrötklgh jaöitjsdbkög sdk lzh jskjödgh aköghp", user1, true
 
-    user3 = overlay.new_user "Darth Vader", "pwDarthVader"
+    user3 = overlay.new_user "Darth Vader", "pwDarthVader", "lord.vader@imperium.com"
     overlay.new_item "Death Star", 10000, "Big ass space ship", user3, true
     overlay.new_item "Storm Trooper", 25, "Do you already own one?", user3, false
     overlay.new_item "Dark Side of the Force", 10, "UNLIMITED POWER!!!", user3, false
 
-    user2 = overlay.new_user "ese", "pwese"
+    user2 = overlay.new_user "ese", "pwese", "kenneth.radunz@web.de"
     overlay.new_item "Nyan Cat", 80, "The ultimate internet cat", user2, true
     overlay.new_item "Overly attached girlfriend", 0, "Take it, it's for free!!!", user2, true
 
-    user4 = overlay.new_user "Steve", "pwSteve"
+    user4 = overlay.new_user "Steve", "pwSteve", "steve@myblock.org"
     overlay.new_item "Dirt", 1, "Minecraft :D", user4, true
     overlay.new_item "Diamond Pickaxe", 75, "Minecraft :D", user4, false
   end
