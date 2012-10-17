a51648dc6c235a004d0fedaa24eab1e006beeaff
Patrick Suter <patrickasuter@gmail.com>
2012-10-17 14:48:09 +0200

    added some exception, still needs a lot of refactoring

--- trade/app/controllers/authentication.rb
+++ trade/app/controllers/authentication.rb
@@ -2,6 +2,7 @@ require_relative '../models/user'
 require_relative '../helpers/image_helper'
 require_relative 'base_controller'
 require_relative '../helpers/user_data_helper'
+require_relative '../models/trade_exception'
 
 class Authentication < BaseController
 
@@ -22,7 +23,7 @@ class Authentication < BaseController
     UserDataHelper.login(params[:username],params[:password])
     session[:name] = params[:username]
     redirect '/index'
-    rescue Exception => e
+    rescue TradeException => e
       add_message(e.message, :error)
       haml :login
     end
@@ -30,7 +31,7 @@ class Authentication < BaseController
 
   #SH Logs the user out
   get "/logout" do
-    session[:name] = nil
+    session.clear
     redirect '/login'
   end
 
@@ -44,31 +45,11 @@ class Authentication < BaseController
 
   #SH Adds an user an redirect to the login page
   post "/register" do
-    if Models::User.passwd_valid?(params[:passwd])
-      if params[:passwd]==params[:passwd_repetition]
-        unless @data.user_exists?(params[:username])
-          display_name = params[:display_name]
-          display_name = UserDataHelper.remove_white_spaces(display_name)
-          if params[:username] == "" || params[:display_name] == "" || params[:passwd] == "" || params[:email] == ""
-            add_message("Data are missing.", :error)
-          else if @data.user_display_name_exists?(display_name)
-            add_message("Display name already exists.", :error)
-          end
-            if !UserDataHelper.right_email?(params[:email])
-              add_message("Incorrect email address.", :error)
-            end
-          end
-          new_user = @data.new_user(params[:username], display_name, params[:passwd], params[:email], params[:interests])
-          new_user.image = ImageHelper.save(params[:image],"#{settings.public_folder}/images/users")
-          add_message("Successful registered.", :success)
-        else
-          add_message("User already exists.", :error)
-        end
-      else
-        add_message("Password and password repetition have to be identical.", :error)
-      end
-    else
-      add_message("Your password is invalid.", :error)
+    begin
+    new_user = UserDataHelper.register(params)
+    add_message("Successfully registered user #{new_user.name}.", :success)
+    rescue TradeException => e
+     add_message(e.message, :error)
     end
     haml :register
   end
--- trade/app/helpers/user_data_helper.rb
+++ trade/app/helpers/user_data_helper.rb
@@ -1,3 +1,5 @@
+require_relative '../models/trade_exception'
+
 class UserDataHelper
 
   def self.remove_white_spaces(display_name)
@@ -12,6 +14,7 @@ class UserDataHelper
   end
 
   def self.right_email?(email)
+    #TODO check that it isn't used yet
     if email.include? '@' and email.include? '.'
       return true
     end
@@ -24,7 +27,43 @@ class UserDataHelper
     password = password
     user = @data.user_by_name name
     if user.nil? || !user.authenticated?(password)
-      raise "Wrong login"
+      raise TradeException, "Wrong login"
+    end
+  end
+
+  def self.register params
+    data = Models::DataOverlay.instance
+    #PS check some stuff
+    self.register_checks(params)
+    #PS create the new user
+    new_user = data.new_user(params[:username], display_name, params[:passwd], params[:email], params[:interests])
+    new_user.image = ImageHelper.save(params[:image],"#{settings.public_folder}/images/users")
+  end
+
+  #PS check for required fields and contents
+  def self.register_checks params
+    data = Models::DataOverlay.instance
+
+    if params[:username] == "" || params[:display_name] == "" || params[:passwd] == "" || params[:email] == ""
+      raise TradeException, "Data are missing."
+    end
+
+    unless Models::User.passwd_valid?(params[:passwd])
+      raise TradeException, "Your password is invalid."
+    end
+
+    if data.user_exists?(params[:username])
+      raise TradeException, "User already exists."
+    end
+
+    unless UserDataHelper.right_email?(params[:email])
+      raise TradeException, "Incorrect email address."
+    end
+
+    display_name = UserDataHelper.remove_white_spaces(params[:display_name])
+
+    if data.user_display_name_exists?(display_name)
+      raise TradeException, "Display name already exists."
     end
   end
 end
