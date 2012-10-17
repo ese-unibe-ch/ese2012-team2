d689794b7bc66f2a9537089e2e22b398eebf472e
Patrick Suter <patrickasuter@gmail.com>
2012-10-17 10:21:56 +0200

    security fix for javascript injection

--- trade/app/controllers/base_controller.rb
+++ trade/app/controllers/base_controller.rb
@@ -1,6 +1,14 @@
 class BaseController < Sinatra::Application
   attr_accessor :message, :title
 
+  before do
+    params.each do |key, param|
+      if param.is_a? String
+        params[key] = Sanitize.clean(param)
+      end
+    end
+  end
+
   def add_message(text, type=:info)
     self.message = {:type => type, :text => text}
   end
